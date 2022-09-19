6663abc7e9671960e19abc32c85a62f3503afc10
Víctor Fernández <victor.fernandez.gabriel@gmail.com>
2022-09-19 16:14:28 +0200

    feat: handle all certificate stuff

--- customize.sh
+++ customize.sh
@@ -33,7 +33,8 @@ REPLACE_EXAMPLE="
 "
 
 # Construct your own list here
-REPLACE="/system/etc/security/cacerts"
+REPLACE="
+"
 
 ##########################################################################################
 # Permissions
@@ -44,16 +45,16 @@ set_permissions() {
 
   # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
   # Some examples:
-
+  
   # For directories (includes files in them):
   # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-
+  
   # set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
   # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
 
   # For files (not in directories taken care of above)
   # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-
+  
   # set_perm $MODPATH/system/lib/libart.so 0 0 0644
   # set_perm /data/local/tmp/file.txt 0 0 644
 }
